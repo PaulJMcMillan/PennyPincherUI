10898fc878cc600bb0948350a94f7d0989e61705
Paul McMillan <paul.mcmillan@procurementpartners.com>
2025-01-16 00:52:39 -0500

    Add project files.

---
+++ PennyPincherUI/Features/Entities/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PennyPincherWebApi.Features.Entity
+{
+    public partial class Entity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool Active { get; set; } = true;
+        public string Notes { get; set; } = string.Empty;
+        public decimal LowestBalance { get; set; }
+        public bool DefaultEntity { get; set; } = false;
+        public int CustomerId { get; set; }
+    }
+}
