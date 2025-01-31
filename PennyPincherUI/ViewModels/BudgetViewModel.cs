9af3d6e608a5a81da253a6c04f7e1d0b8a12980f
Paul McMillan <paul.mcmillan@procurementpartners.com>
2025-01-30 23:00:58 -0500

    Added Frequency Enum.

---
+++ PennyPincherUI/ViewModels/BudgetViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PennyPincherUI.ViewModels
+{
+    public class BudgetViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public int FrequencyId { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime PayDate { get; set; }
+        public string Notes { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int EntityId { get; set; }
+        public int NumberOfPayments { get; set; }
+        public string FrequencyName { get; set; } = string.Empty;
+    }
+}
